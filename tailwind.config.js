f0208621702ac5b0e81875403a6166a9f25e5fa9
Nick K <nick@arguflow.gg>
2024-02-05 21:57:22 -0800

    design: set font as Sen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Sen", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
