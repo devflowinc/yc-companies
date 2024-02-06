90a7ecfe15a5f1c6fb9138ea7fe514f273cc2900
Nick K <nick@arguflow.gg>
2024-02-05 22:43:10 -0800

    styling: skeleton styling up to search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       sans: ["Sen", ...defaultTheme.fontFamily.sans],
     },
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
